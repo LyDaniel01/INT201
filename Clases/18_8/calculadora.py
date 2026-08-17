96b934468ad7701b07f67226b07b036c3d6f5b1a
lyDaniel01 <dlaymerodas@gmail.com>
2026-08-17 15:39:37 -0400

    calculadora en python

---
+++ Clases/18_8/calculadora.py
@@ -0,0 +1,26 @@
+a = int(input("INtroduzca el primer digito "))
+op =input ("Introduzca el operador ")
+b = int (input("INtroduzca el segundo digito "))
+
+if op == "+":
+    c = a + b
+    print(c)
+    
+elif (op == "-"):
+    c = a- b
+    print  (c)
+    
+elif (op == "*"):
+    c = a * b
+    print(c)
+    
+elif (op == "/"):
+    c = a / b
+    print (c)
+    
+elif (op == "^"):
+    c = a ** b
+    print(c)
+
+
+
